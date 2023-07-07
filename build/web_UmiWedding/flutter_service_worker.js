9c2b439de876df343856d4f6caa3a79815680eed
alhaadidol@gmail.com <alhaadi@idb.com.my>
2023-07-07 22:56:43 +0800

    1. add wedding umi mobile support view_ 2

--- build/web_UmiWedding/flutter_service_worker.js
+++ build/web_UmiWedding/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4aa78f0c7b0a33a604db17d5f6893baa",
-"index.html": "ceef329ee38c5fa573b331d1406e7c61",
-"/": "ceef329ee38c5fa573b331d1406e7c61",
+"index.html": "a428ae2e804901b2fe3123f9473fca3d",
+"/": "a428ae2e804901b2fe3123f9473fca3d",
 "main.dart.js": "f8b8915e227fda55d8f62b4d6d0c0105",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "cec06261144dd5f33864440510a04c82",
